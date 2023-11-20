ca51e2040513bf1388447a94ee0a50eea540045d
Kevin Cazelles <kevin.cazelles@insileco.io>
2023-11-20 06:49:26 -0500

    testpkg02 depends on testpkg01

---
+++ testpkg02/R/call_hello_co2.R
@@ -0,0 +1,7 @@
+#' Call hello_co2() from `testpkg01`
+#' @export
+#' @examples
+#' call_hello_co2()
+call_hello_co2 <- function() {
+    testpkg01::hello_co2()
+}
--- testpkg02/R/seqRow.R
+++
@@ -1,6 +0,0 @@
-#' Sequence based on the number of row of a dataframe
-#' @param x a dataframe.
-#' @export
-seqRow <- function(x) {
-    seq_len(NROW(x))
-}
